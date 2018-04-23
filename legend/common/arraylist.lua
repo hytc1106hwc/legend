98e57e72b31c7739b35d4b5241aa820bd4cf5616
hwc <1729116521@qq.com>
2018-04-23 17:42:30 +0800

    add basic collection module

---
+++ legend/common/arraylist.lua
@@ -0,0 +1,15 @@
+---------------------------------------------------------------------------------------
+-- @function    ArrayList模块
+-- @author      ArisHu
+-- @Note        
+---------------------------------------------------------------------------------------
+local base = _G
+local table = require("table")
+local string = require("string")
+local math = require("math")
+local legend = require("legend")
+local collection = require("collection")
+local class = require("class")
+local list = require("list")
+local collection = require()
+
